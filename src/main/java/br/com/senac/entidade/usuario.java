adad46ae4141f0cc27756a727f22a5d271991773
Lucasp20 <lucaspereira_lp@hotmail.com>
2021-06-25 20:08:59 -0300

    Ajuste da entidade usuário

--- src/main/java/br/com/senac/entidade/usuario.java
+++ src/main/java/br/com/senac/entidade/usuario.java
@@ -22,25 +22,26 @@ public class usuario implements Serializable {
 	@Column(nullable = false)
 	private String nome;
 	
-	public usuario(Long id, String nome) {
+	@Column(nullable = false, unique = true)
+	private String login;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	
+
+	public usuario(String nome, String login, String senha) {
 		super();
-		this.id = id;
 		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
 	}
 	
 	public usuario() {
 		super();
 		
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -49,4 +50,45 @@ public class usuario implements Serializable {
 		this.nome = nome;
 	}
 
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		usuario other = (usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
