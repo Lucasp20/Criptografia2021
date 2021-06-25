e9e7f4f5b1df25c4dc2f29605794b94be0386883
Lucasp20 <lucaspereira_lp@hotmail.com>
2021-06-25 19:28:48 -0300

    Criando entidade, dao e util

---
+++ br/com/senac/dao/HibernateUtil.java
@@ -0,0 +1,34 @@
+package br.com.senac.dao;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import br.com.senac.entidade.*;
+
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            Configuration cfg = new Configuration();
+            cfg.addAnnotatedClass(usuario.class);
+             
+               
+            
+            cfg.configure("/br/com/senac/dao/hibernate.cfg.xml");
+            StandardServiceRegistryBuilder build = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
+            sessionFactory = cfg.buildSessionFactory(build.build());
+        } catch (HibernateException ex) {
+            System.err.println("Erro ao criar Hibernate util." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static Session abrirSessao() {
+        return sessionFactory.openSession();
+    }
+}
---
+++ src/main/java/br/com/senac/entidade/usuario.java
@@ -0,0 +1,52 @@
+package br.com.senac.entidade;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	public usuario(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public usuario() {
+		super();
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
